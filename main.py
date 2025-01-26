e514830893b84da21378d2a0f1bdd3be2a0e1aa9
Juan Luis Cano Rodríguez <juan_luis_cano@mckinsey.com>
2025-01-26 22:36:28 +0100

    Add alternative function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from math import pi
+import numpy as np
 
-from rode import euler_method_demo_py
+from rode import euler_method_demo_py, euler_method_demo_full_py
 
 
 def y(x):
@@ -9,10 +9,18 @@ def y(x):
 
 if __name__ == "__main__":
     y0_demo = 1.0
-    t_start_demo = 0.0
-    t_end_demo = 2.0 * pi
-    num_points_demo = 100
-    results = euler_method_demo_py(y, y0_demo, t_start_demo, t_end_demo, num_points_demo)
+    t_start = 0.0
+    t_end = 2.0 * np.pi
+    num_points = 100
+    results = euler_method_demo_py(y, y0_demo, t_start, t_end, num_points)
+
+    print(type(results))
+    print(results)
+
+    t = np.linspace(t_start, t_end, num_points)
+    h = t[1] - t[0]
+
+    results = euler_method_demo_full_py(y, y0_demo, t, h)
 
     print(type(results))
     print(results)
