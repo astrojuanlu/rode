b95a776e32b60d0ac302f91489bcaa1ad192b050
Juan Luis Cano Rodríguez <juan_luis_cano@mckinsey.com>
2025-01-26 19:36:14 +0100

    First commit, basic structure

---
+++ rust/src/lib.rs
@@ -0,0 +1,38 @@
+use ndarray::Array1;
+use pyo3::prelude::*;
+use std::f64::consts::PI;
+
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+/// Implements the Euler method to solve ordinary differential equations.
+pub fn euler_method<F>(dx_dt: F, y0: f64, t: &Array1<f64>, h: f64) -> Array1<f64>
+where
+    F: Fn(f64) -> f64,
+{
+    let mut y = Array1::zeros(t.len());
+    y[0] = y0;
+    for i in 1..t.len() {
+        y[i] = y[i - 1] + dx_dt(y[i - 1]) * h;
+    }
+    y
+}
+
+/// A demo function that calls euler_method with a dummy function.
+pub fn euler_method_demo(y0: f64, t_start: f64, t_end: f64, num_points: usize) -> Array1<f64> {
+    let dx_dt = |x: f64| -> f64 { -x };
+    let t = Array1::linspace(t_start, t_end, num_points);
+    let h = t[1] - t[0];
+    euler_method(dx_dt, y0, &t, h)
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+#[pyo3(name = "_rode")]
+fn rode(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    Ok(())
+}
