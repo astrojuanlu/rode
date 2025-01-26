933d339dc64d7980245e303efaddc204d0717698
Juan Luis Cano Rodríguez <juan_luis_cano@mckinsey.com>
2025-01-26 20:20:40 +0100

    Calling Python functions back works!

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,6 +1,7 @@
 use numpy::ndarray::Array1;
 use numpy::{IntoPyArray, PyArray1};
 use pyo3::prelude::*;
+use pyo3::types::PyAny;
 
 /// Implements the Euler method to solve ordinary differential equations.
 pub fn euler_method<F>(dx_dt: F, y0: f64, t: &Array1<f64>, h: f64) -> Array1<f64>
@@ -35,13 +36,17 @@ where
 #[pyfunction]
 fn euler_method_demo_py(
     py: Python,
+    dx_dt: Py<PyAny>,
     y0: f64,
     t_start: f64,
     t_end: f64,
     num_points: usize,
 ) -> PyResult<Bound<'_, PyArray1<f64>>> {
-    let dx_dt = |x: f64| -> f64 { -x };
-    let result = euler_method_demo(dx_dt, y0, t_start, t_end, num_points);
+    let dx_dt_closure = |x: f64| -> f64 {
+        let args = (x,);
+        dx_dt.call1(py, args).unwrap().extract(py).unwrap()
+    };
+    let result = euler_method_demo(dx_dt_closure, y0, t_start, t_end, num_points);
     Ok(result.into_pyarray(py))
 }
 
