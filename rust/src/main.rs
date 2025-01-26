75e33536d6c45ca94f1df2ec037e5e5ecb280111
Juan Luis Cano Rodríguez <juan_luis_cano@mckinsey.com>
2025-01-26 19:58:48 +0100

    Cleanup

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,7 +1,7 @@
 use numpy::ndarray::Array1;
 use std::f64::consts::PI;
 
-use rode::{euler_method, euler_method_demo};
+use rode::euler_method;
 
 fn main() {
     // Demonstrate euler_method
@@ -15,19 +15,5 @@ fn main() {
     println!("Results from euler_method:");
     for (i, &value) in result.iter().enumerate() {
         println!("t = {:.2}, y = {:.5}", t[i], value);
-    }
-
-    // Demonstrate euler_method_demo
-    let y0_demo = 1.0;
-    let t_start_demo = 0.0;
-    let t_end_demo = 2.0 * PI;
-    let num_points_demo = 100;
-
-    let result_demo = euler_method_demo(y0_demo, t_start_demo, t_end_demo, num_points_demo);
-
-    println!("\nResults from euler_method_demo:");
-    let t_demo = Array1::linspace(t_start_demo, t_end_demo, num_points_demo);
-    for (i, &value) in result_demo.iter().enumerate() {
-        println!("t = {:.2}, y = {:.5}", t_demo[i], value);
     }
 }
